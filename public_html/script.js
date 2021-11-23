49725308f7a7741e5b6ffebe47b1f2bd3c4e7a51
Bryan-Brumley <90947317+Bryan-Brumley@users.noreply.github.com>
2021-11-23 05:36:32 -0600

    more work on css

--- public_html/script.js
+++ public_html/script.js
@@ -4,8 +4,7 @@ $(document).ready(function () {
         errorClass: "alert alert-danger",
         errorLabelContainer: "#output-area",
         errorElement: "div",
-        // rules here define what is good or bad input
-        // each rule starts eith the form input element's name attribute
+
         rules:{
             name: {
                 required: true
@@ -13,7 +12,7 @@ $(document).ready(function () {
             email: {
                 email: true,
                 required: true
-            } ,
+            },
             message: {
                 required: true,
                 maxlength: 2000,
@@ -22,25 +21,25 @@ $(document).ready(function () {
         messages:{
             name: {
                 required: "Name is a required field"
-            }
+            },
             email: {
-                email: "Please enter a valid email address",
+                email: "Please provide a valid email address",
                 required: "Email is a required field"
             },
             message: {
                 required: "Message is a required field",
-                maxlength: "Message is too long"
+                maxlength: "Message is to long"
             }
         },
-        submitHandler: (<form) => {
+        submitHandler: (form) => {
             $("#contact").ajaxSubmit({
                 type: "POST",
                 url: $("#contact").attr('action'),
                 success: (ajaxOutput) => {
-                    $("output-area").css("display", "")
-                    $("output-area").html(ajaxOutput)
+                    $("#output-area").css("display", "")
+                    $("#output-area").html(ajaxOutput)
                     if($(".alert-success" >= 1)) {
-                        $("contact")[0]/reset()
+                        $("#contact")[0].reset()
                     }
                 }
             })
